20c14f88e27a03ab607bba2a426b6cf1dd3fb198
MelissaRivas494 <m.rivascofre@gmail.com>
2024-10-05 16:19:29 -0300

    add estilos cards, navbar, carrusel

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,4 @@ document.addEventListener('DOMContentLoaded', function () {
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 });
+
